a69f3bed7d89d9aef4ab72a58c8a808d5db20499
vigneshcodez <rvignesh565@gmail.com>
2025-03-04 17:21:32 +0530

    after supply media file

--- project/settings.py
+++ project/settings.py
@@ -129,6 +129,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'assets'
 ]
 
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
