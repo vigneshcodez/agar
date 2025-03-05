a3f1ded133ff3ed6e7f6708c72dbac28dc9b72d9
vigneshcodez <rvignesh565@gmail.com>
2025-03-05 11:15:20 +0530

    after login required

--- app/views.py
+++ app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='login')
 def index(request):
     return render(request,'app_templates/index.html')
 
+@login_required(login_url='login')
 def about(request):
     return render(request,'app_templates/about.html')
 
